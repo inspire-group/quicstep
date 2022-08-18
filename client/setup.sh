1f8c4deca255a21ab97630b3db8e0ae9f130c562
Watson <watsonj@princeton.edu>
2022-08-18 15:59:13 -0700

    add config and setup scripts

---
+++ client/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# disable IPv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+wg-quick up wg0
