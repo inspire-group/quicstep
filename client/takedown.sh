7eec5910f104e5d39e2b3b7dc40a8790e4292a81
Watson <watsonj@princeton.edu>
2023-10-30 20:02:19 -0400

    add takedown script

---
+++ client/takedown.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# enable IPv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=0
+sysctl -w net.ipv6.conf.default.disable_ipv6=0
+sysctl -w net.ipv6.conf.lo.disable_ipv6=0
+
+wg-quick down wg0
